18b11c1fc17061906dce041fd3759f0fe8ca2b7f
apalombaro <alexpalombaro@gmail.com>
2015-09-01 15:37:44 +0800

    Routine commit

---
+++ src/views/home/Home.spec.js
@@ -0,0 +1,9 @@
+/* global describe it expect */
+
+import React from 'react';
+import HomeView from './Home.spec.js';
+
+describe('(View) Home', function () {
+  it('Should have a test that works with Chai expectations.', function () {
+  });
+});
